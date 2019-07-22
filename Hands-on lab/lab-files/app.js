cdc20da28108115598d4e05116175cc8b5d94efe
Stefan Amyotte <stefan.amyotte@gmail.com>
2019-07-22 13:01:50 -0400

    docker config

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://best-for-you-db-sa:LksDbHpVSVkGrO8vbMwJOYBlSgEa5URBqMXZeHjorbJgyuuoD1nsdxBXNAluENz3R3Vgt2ZCGyaepeQ8gL1Ayw==@best-for-you-db-sa.documents.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
